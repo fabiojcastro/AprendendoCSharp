f796b9956eb69184cda760a8753021f19edb9cbb
fabiojcastro <fabio23.castro@gmail.com>
2021-08-03 16:29:19 -0300

    Meu Primeiro Projeto

---
+++ 1_PrimeiroProjeto/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1_PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, it's my firt project!");
+            Console.WriteLine("For to finish the program, press Enter please. . .");
+            Console.ReadLine();
+        }
+    }
+}
