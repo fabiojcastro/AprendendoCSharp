4ef592e2ba1622383f6496f4a06b8d8c6580deaf
fabiojcastro <fabio23.castro@gmail.com>
2021-08-03 17:23:56 -0300

    Praticando commit

---
+++ 3_VariaveisFlutuantes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _3_VariaveisFlutuantes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aprendendo a lidar com pontos flutuantes.");
+
+            double salario, semanal;
+
+            salario = 2500.70;
+
+            semanal = salario / 4;
+                Console.WriteLine(salario);
+
+            Console.WriteLine("Meu pagamento semanal é de " + semanal);
+
+            Console.ReadLine();
+        }
+    }
+}
