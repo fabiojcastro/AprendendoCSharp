cde604935a70573c7dd461930b6b23bf8ab6d290
fabiojcastro <fabio23.castro@gmail.com>
2021-08-03 16:52:27 -0300

    Segundo Projeto

---
+++ 2_CriandoVariaveis/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Praticando na inserção de variáveis.");
+            Console.WriteLine();
+            
+            int idade;
+            
+            idade = 29;
+            Console.WriteLine(idade);
+
+            idade = 10 + 13;
+            Console.WriteLine(idade);
+
+            idade = 10 + 2 * 5;
+            Console.WriteLine(idade);
+
+            idade = 50 - 10 / 2;
+            Console.WriteLine(idade);
+            Console.WriteLine();
+
+            Console.WriteLine("Minha idade é " + idade + " anos!");
+            Console.ReadLine();
+        }
+    }
+}
