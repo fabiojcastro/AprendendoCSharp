d335f04959d623373755b077285ede945e83819a
fabiojcastro <fabio23.castro@gmail.com>
2021-08-04 21:33:16 -0300

    Praticando na criação de novos projetos

---
+++ 4_CriandoVariaveis/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _4_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 4_Criando_Variaveis");
+            
+            // int é um tipo de variavel que aceita numeros inteiros até 64 bits.
+            int salario = 2800;
+            Console.WriteLine(salario);
+
+            // double é um tipo de váriavel que aceita números com casas decimais até 64 bits.
+            double distancia = 1200.73;
+            Console.WriteLine(distancia);
+
+            //long é um tipo de váriável que aceita números inteiros ate 64 bits;
+            long lotes = 15000000;
+            Console.WriteLine(lotes);
+
+            //type casting
+            int comprimento = (int)distancia;
+            Console.WriteLine(comprimento);
+
+            // short é um tipo de variavel que aceita numeros inteiros até 16 bits
+            short compras = 15000;
+            Console.WriteLine(compras);
+
+            // float é um tipo de váriavel que aceita números decimais até 16 bits
+            float altura = 1.70f;
+            Console.WriteLine(altura);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ 5_CaracteresTextos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _5_CaracteresTextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 5 Caracteres e Textos");
+
+            char primeiraletra = 'F';
+            Console.WriteLine(primeiraletra);
+
+            // char entende números como caracteres ASCII.
+            primeiraletra = (char)61;
+            Console.WriteLine(primeiraletra);
+
+            primeiraletra = (char)(primeiraletra + 1);
+            Console.WriteLine(primeiraletra);
+
+            string titulo = "Alura cursos de tecnologia " + 2021;
+            Console.WriteLine(titulo);
+
+            Console.ReadLine();
+        }
+    }
+}
